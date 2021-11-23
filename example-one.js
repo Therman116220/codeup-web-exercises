f549f67f36b4744933072359efbb87d5ecaed280
Therman116220 <thermanhaynes@gmail.com>
2021-11-23 12:28:02 -0600

    "having a little trouble cleaning up here, will get better"

---
+++ example-one.js
@@ -0,0 +1,23 @@
+//Primitive Types//
+
+let  exerciseone = 99.9;
+console.log(typeof exerciseone);
+//string//
+let  exercisetwo = 'false';
+console.log(typeof exercisetwo);
+//string//
+let  exercisethree = false;
+console.log(typeof exercisethree);
+//string//
+let  exercisefour = '0';
+console.log(typeof exercisefour);
+//string//
+let  exercisefive = 0;
+console.log(typeof exercisefive);
+//number//
+let  exercisesix = true == true;
+console.log(typeof exercisesix);
+//boolian//
+let  exerciseseven = 'true';
+console.log(typeof exerciseseven);
+//string//
