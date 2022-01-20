a8b286e74d7a2f574db88a6b6df2a90713e5a820
Therman116220 <thermanhaynes@gmail.com>
2022-01-20 09:08:09 -0600

    completed

---
+++ js/jquery.js
@@ -0,0 +1,31 @@
+/*https://api.jquery.com/*/
+
+
+/*
+$(document).ready(function(){
+    $("hidden").hover(function() {
+
+    })
+})
+*/
+/*
+document.getElementById("heading");
+$()*/
+
+
+
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+
+
+alert("The DOM has finished loading");
+
+
+
+
+
+
+
+
+
